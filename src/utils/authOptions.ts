6a50176ccb3096abcaef8898e46c20a806100a39
Muslehud77 <fardinmohit@gmail.com>
2024-10-08 21:08:32 +0800

    Configuring nextauth

---
+++ src/utils/authOptions.ts
@@ -0,0 +1,10 @@
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions:NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+   
+  ],
+};
+
