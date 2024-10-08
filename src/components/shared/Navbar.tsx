a21c63f1ef946343364d4d07512a16f5c149bb9d
Muslehud77 <fardinmohit@gmail.com>
2024-10-09 00:39:02 +0800

    Configured github login with the session

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,6 +1,17 @@
+"use client"
+import { Session } from "next-auth";
+import { signOut } from "next-auth/react";
 import Link from "next/link";
 
-const Navbar = () => {
+
+type TNavbarProps = {
+  session: Session;
+}
+
+const Navbar = ({session}:TNavbarProps) => {
+
+  console.log(session);
+
   return (
     <div className="navbar bg-base-100  border-b  w-[90%] mx-auto">
       <div className="navbar-start">
@@ -59,16 +70,23 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="navbar-end">
-        <button className="btn btn-error btn-outline text-white rounded-full px-5">
-          Logout
-        </button>
-
-        <Link
-          href="/login"
-          className="btn btn-accent btn-outline text-white rounded-full px-5"
-        >
-          Login
-        </Link>
+        {session?.user ? (
+          <>
+            
+            <button onClick={()=>signOut()} className="btn btn-error btn-outline text-white rounded-full px-5">
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <Link
+              href="/login"
+              className="btn btn-accent btn-outline text-white rounded-full px-5"
+            >
+              Login
+            </Link>
+          </>
+        )}
       </div>
     </div>
   );
